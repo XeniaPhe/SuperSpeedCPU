b90edb02dd88e4c68031f34e820a18064e80989b
XeniaPhe <m.tunahanbas90@gmail.com>
2023-01-06 20:42:46 +0300

    Some progress on ram

---
+++ Verilog/Memory/ram.v
@@ -0,0 +1,24 @@
+module ram(
+    input [9:0] addr,
+    input [19:0] write,
+    input clk, str, ld,
+    output reg [19:0] read);
+
+    reg [19:0] mem [0:1023];
+
+    integer i;
+    initial begin
+        for(i = 0; i < 1024; i = i + 1) begin
+            mem[i] = 20'd0;
+        end
+    end
+
+    always @(posedge clk) begin
+        if(str)
+            mem[addr] <= write;
+    end
+
+    always @(*) begin
+        read = ld ? mem[addr] : 20'dx;
+    end
+endmodule
---
+++ Verilog/Memory/ram_tb.v
@@ -0,0 +1,46 @@
+`timescale 1ns/1ns
+`include "Memory/ram.v"
+
+module ram_tb;
+    reg [9:0] addr;
+    reg [19:0] write;
+    reg clk, str, ld;
+    wire [19:0] read;
+
+    ram mem(addr, write, clk, str, ld, read);
+
+    initial begin
+        clk = 0;
+        #10
+        forever #5 clk = ~clk;
+    end
+
+    initial begin
+        $dumpfile("ram_tb.vcd");
+        $dumpvars(0,ram_tb);
+
+        ld = 1;
+        addr = 0; write = 101; str = 0;
+        #10
+        str = 1;
+        #10
+
+        ld = 0;
+        #10
+
+        ld = 1;
+        addr = 1; write = 202;
+        #10
+
+        addr = 2; write = 303;
+        #10
+
+        addr = 3; write = 404;
+        #10
+
+        addr = 4; write = 505;
+        #10
+
+        $finish;
+    end
+endmodule
